4dffa823f9954d522c1f4bcc4b14f5f36f846fb6
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 21:01:09 +0530

    Add MethodSecurityConfig import for enhanced security configuration

--- src/main/java/com/shah_s/bakery_order_service/BakeryOrderServiceApplication.java
+++ src/main/java/com/shah_s/bakery_order_service/BakeryOrderServiceApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import org.springframework.context.annotation.Import;
+import org.devofblue.common.security.MethodSecurityConfig;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(MethodSecurityConfig.class)
 @EnableFeignClients
 public class BakeryOrderServiceApplication {
 
